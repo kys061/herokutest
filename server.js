481ffd985c10e07b3cb5d65a1a8a4a8d3d6abe41
yskang <kys061@gmail.com>
2015-10-21 15:03:45 +0900

    to test heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express()
+
+app.use('/', express.static('./'));
+
+//app.get('/', function(req, res){
+    //res.send("index.html");
+//})
+
+var server = app.listen(3333, function(){
+  console.log('server running http://localhost:' + server.address().port)
+});
